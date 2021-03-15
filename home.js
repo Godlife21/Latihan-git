b7d65579daaded08e2f22e3a57ebb485e0254ff0
Godlife21 <62981120+Godlife21@users.noreply.github.com>
2021-03-15 17:15:40 +0800

    tambah baris kedelapan

--- home.js
+++ home.js
@@ -6,3 +6,9 @@ console.log("Baris kedua");
 
 console.log("Baris ketiga");
 console.log("Baris keempat dari online");
+console.log("Baris kelima");
+console.log("Baris keenam");
+console.log("Baris Kejutuh);
+            
+//Jumat
+console.log("Baris kedelapan");
